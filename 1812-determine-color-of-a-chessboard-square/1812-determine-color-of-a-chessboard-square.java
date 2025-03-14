8fb23180211f47896f545e275bd836f60808018a
Pratik Patil <pratikpatil2069@gmail.com>
2025-03-14 14:52:20 +0530

    Time: 0 ms (100%), Space: 41.2 MB (89.41%) - LeetHub

---
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        if(coordinates.equals("a1")||coordinates.equals("a3")||coordinates.equals("a5")||coordinates.equals("a7")||coordinates.equals("b2")||coordinates.equals("b4")||coordinates.equals("b6")||coordinates.equals("b8")||coordinates.equals("c1")||coordinates.equals("c3")||coordinates.equals("c5")||coordinates.equals("c7")||coordinates.equals("d2")||coordinates.equals("d4")||coordinates.equals("d6")||coordinates.equals("d8")||coordinates.equals("e1")||coordinates.equals("e3")||coordinates.equals("e5")||coordinates.equals("e7")||coordinates.equals("f2")||coordinates.equals("f4")||coordinates.equals("f6")||coordinates.equals("f8")||coordinates.equals("g1")||coordinates.equals("g3")||coordinates.equals("g5")||coordinates.equals("g7")||coordinates.equals("h2")||coordinates.equals("h4")||coordinates.equals("h6")||coordinates.equals("h8")){
+            return false;
+        }else{
+            return true;
+        }
+        
+        
+    }
+}

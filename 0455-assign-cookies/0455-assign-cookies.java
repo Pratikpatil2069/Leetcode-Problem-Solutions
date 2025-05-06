ca4b7a815bceea6d113736fb70aec36b9f21e3f0
Pratik Patil <pratikpatil2069@gmail.com>
2025-05-05 21:57:54 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+       Arrays.sort(g);
+       Arrays.sort(s);
+       int left1=0;
+       int left2=0;
+       int count=0;
+       while(left1!=s.length-1){
+        if(s[left1]>=g[left2]){
+            count++;
+            left1++;
+            left2++;
+        }else if(left2==g.length){
+            left2=0;
+            left1++;
+        }else{
+            left2++;
+        }
+       }
+       return count;
+    }
+}

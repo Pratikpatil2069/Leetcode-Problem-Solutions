540ca1114966b0c1a84f8bb08af1b20960568e43
Pratik Patil <pratikpatil2069@gmail.com>
2025-05-19 08:31:42 -0700

    Time: 1 ms (92.83%), Space: 46.2 MB (8.04%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+     int leftSum[]=new int [nums.length];
+     int rightSum[]=new int [nums.length];
+     for(int i=0;i<nums.length;i++){
+        if(i==0){
+            leftSum[i]=0+nums[i];
+        }else{
+            leftSum[i]=leftSum[i-1]+nums[i];
+        }
+     }
+     for(int i=nums.length-1;i>=0;i--){
+       if(i==nums.length-1){
+         rightSum[i]=0+nums[i];
+       }else{
+         rightSum[i]=rightSum[i+1]+nums[i];
+       }
+     }
+    for(int i=0;i<nums.length;i++){
+        if(leftSum[i]==rightSum[i]){
+            return i;
+        }
+    }
+     return -1;
+    }
+}

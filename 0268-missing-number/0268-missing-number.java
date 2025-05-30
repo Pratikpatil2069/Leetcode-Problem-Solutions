73b5899ef7ab9b26c1a9a9388425f1bd7cbababa
Pratik Patil <pratikpatil2069@gmail.com>
2025-05-30 04:31:07 -0700

    Time: 5 ms (25.5%), Space: 45.8 MB (7.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         HashSet<Integer>set=new HashSet<>();
+      
         for(int i=0;i<nums.length;i++){
             set.add(nums[i]);
         }

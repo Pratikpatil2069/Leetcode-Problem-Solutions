ce89011d795fe0bc90dbd2b968ff5a4bbde57e78
Pratik Patil <pratikpatil2069@gmail.com>
2025-06-14 05:29:18 -0700

    Time: 3 ms (62.27%), Space: 44.5 MB (33.29%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -1,18 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Set<Integer>set=new HashSet<>();
-        for(int i=0;i<seats.length;i++){
-            set.add(seats[i]);
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int moves = 0;
+        for (int i = 0; i < seats.length; i++) {
+            moves += Math.abs(seats[i] - students[i]);
         }
-        int n=set.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum=sum+students[i];
-        }
-        int sum1=0;
-        for(int j:set){
-            sum1=sum1+j;
-        }
-        return Math.abs(sum-sum1);
+        return moves;
     }
 }

b901bd6ab94c09cabe062ed303dd7df2d9950fe1
Pratik Patil <pratikpatil2069@gmail.com>
2025-06-19 22:49:27 -0700

    Time: 6 ms (56.33%), Space: 45.3 MB (14.27%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -2,6 +2,7 @@ class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         Set<Integer>set=new HashSet<>();
         Set<Integer>set2=new HashSet<>();
+        Set<Integer>set3=new HashSet<>();
         List<Integer>list=new ArrayList<>();
         for(int i=0;i<nums1.length;i++){
             set.add(nums1[i]);
@@ -9,19 +10,24 @@ class Solution {
         for(int i=0;i<nums2.length;i++){
             set2.add(nums2[i]);
         }
-        for(int i=0;i<nums3.length;i++){
-            if(set.contains(nums3[i])){
-                list.add(nums3[i]);
-                set.remove(nums3[i]);
-            }else if(set2.contains(nums3[i])){
-                list.add(nums3[i]);
-                set2.remove(nums3[i]);
-            }
+          for(int i=0;i<nums3.length;i++){
+            set3.add(nums3[i]);
         }
-        for(Integer num:set){
-            if(set2.contains(num)){
-                list.add(num);
-            }
+        HashMap<Integer,Integer>map=new HashMap<>();
+       for(Integer num:set){
+        map.put(num,map.getOrDefault(num,0)+1);
+       }
+        for(Integer num:set2){
+        map.put(num,map.getOrDefault(num,0)+1);
+       }
+        for(Integer num:set3){
+        map.put(num,map.getOrDefault(num,0)+1);
+       }
+       Set<Integer>set4=map.keySet();
+        for(Integer num:set4){
+           if(map.get(num)>=2){
+            list.add(num);
+           }
         }
         return list;
     }

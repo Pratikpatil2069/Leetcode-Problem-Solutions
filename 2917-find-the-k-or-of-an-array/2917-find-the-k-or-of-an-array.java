aa9ffe17c006d641787293ef74362a33d0dedabf
Pratik Patil <pratikpatil2069@gmail.com>
2025-06-22 21:52:30 -0700

    Time: 8 ms (8.33%), Space: 44.3 MB (30.83%) - LeetHub

---
+++ 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findKOr(int[] nums, int k) {
+        String arr[] = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = Integer.toBinaryString(nums[i]);
+        }
+
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i].length()) {
+                max = arr[i].length();
+            }
+        }
+
+        String res = "";
+        int count = 0;
+
+        for (int i = 0; i < max; i++) {
+            count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                // ✅ FIX: Access bits from the right (least significant bit)
+                int bitIndexFromRight = arr[j].length() - 1 - i;
+                if (bitIndexFromRight >= 0 && arr[j].charAt(bitIndexFromRight) == '1') {
+                    count++;
+                }
+            }
+
+            if (count >= k) {
+                res = res + "1";
+            } else {
+                res = res + "0";
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(res);
+        sb.reverse(); // ✅ FIX: Reverse to get the correct binary order
+        String s = sb.toString();
+
+        return Integer.parseInt(s, 2);
+    }
+}

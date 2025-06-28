0f335f0d0d5dbe24f648ba05ab4dad0f4f80e7f3
Pratik Patil <pratikpatil2069@gmail.com>
2025-06-28 00:22:55 -0700

    Time: 0 ms (100%), Space: 41.1 MB (37.95%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,16 +3,7 @@ class Solution {
         if(n<=0){
             return false;
         }
-        int count=0;
-        while(n>0){
-            if((n&1)==1){
-                count++;
-            }
-            if(count==2){
-                return false;
-            }
-            n=n>>1;
-        }
-        return true;
+      return (n & (n - 1)) == 0;
+
     }
 }

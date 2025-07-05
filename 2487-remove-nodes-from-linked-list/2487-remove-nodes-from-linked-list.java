00c219f42acbfff4b23e891389c2def5b53411b7
Pratik Patil <pratikpatil2069@gmail.com>
2025-07-05 10:38:02 +0530

    Time: 83 ms (5.16%), Space: 68.2 MB (67.13%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -10,35 +10,32 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        ListNode ptr=head;
-        ListNode temp=head.next;
-        boolean flag=true;
-        while(temp!=null){
-            if(ptr.val<=temp.val){
-                ptr=ptr.next;
-                temp=temp.next;
-            }else{
-                if(flag){
-                    head=ptr;
-                    flag=false;
-                }else{
-                    if(head.val<ptr.val){
-                        head=ptr;
-                    }else{
-                    ListNode temp2=head;
-                    temp2.next=ptr;
-                    temp2=temp.next;
-                    }
-                }
-                
-                ptr=ptr.next;
-                temp=temp.next;
-            }
-        }
-        if(flag){
+        if(head==null || head.next==null){
             return head;
         }
-        head.next=ptr;
+        ListNode temp=head;
+        Stack<Integer>stack=new Stack<>();
+            while(temp!=null){
+            if(stack.isEmpty() || temp.val<=stack.peek()){
+                stack.push(temp.val);
+                temp=temp.next;
+            }else{
+                if(stack.isEmpty()){
+                    stack.push(temp.val);
+                    temp=temp.next;
+                }else{
+                stack.pop();
+                }
+            }
+        }
+        head=null;
+        while(!stack.isEmpty()){
+            int val=stack.peek();
+            ListNode node=new ListNode(val);
+            node.next=head;
+            head=node;
+            stack.pop();
+        }
         return head;
     }
 }

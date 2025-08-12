90f207e16526836355a98e35da19e291ed3cbba0
Pratik Patil <pratikpatil2069@gmail.com>
2025-08-12 19:24:00 +0530

    Time: 1 ms (81.59%), Space: 40.6 MB (94.4%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int reverse(int x) {
+        boolean flag=false;
+        if(x<0){
+            x=x*(-1);
+            flag=true;
+        }
+        int num=0;
+        while(x>0){
+            int re=x%10;
+            if (num > Integer.MAX_VALUE / 10 || 
+               (num == Integer.MAX_VALUE / 10 && re > 7)) {
+                return 0;
+            }
+            num=num*10+re;
+            
+       
+        
+            x=x/10;
+        }
+        if(flag){
+            num=num*(-1);
+        }
+       
+        return num;
+    }
+}

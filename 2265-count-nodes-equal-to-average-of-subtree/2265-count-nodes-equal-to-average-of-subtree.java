f4ddb8cb9e839ba12fb3b4ae72e602923289e76f
Pratik Patil <pratikpatil2069@gmail.com>
2025-09-18 20:29:58 +0530

    Time: 0 ms (100%), Space: 44.4 MB (44.23%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -16,35 +16,21 @@
 class Solution {
     public int averageOfSubtree(TreeNode root) {
     int count[]=new int [1];
-       avg(root,count);
+       countNode(root,count);
        return count[0];
     }
-    public void avg(TreeNode root,int[] count){
-        if(root==null){
-            return;
-        }
-        if(root.left!=null){
-            avg(root.left,count);
-        }
-        int AVG[]=countNode(root);
-        if(AVG[1]/AVG[0]==root.val){
-            count[0]++;
-        }
-        if(root.right!=null){
-            avg(root.right,count);
-        }       
-    }
-    public int[] countNode(TreeNode root){
+
+    public int[] countNode(TreeNode root,int[] count){
         if(root==null){
             return new int[]{0,0};
         }
-            int[] leftSum=countNode(root.left);
-        
-        
-            int[] rightSum=countNode(root.right);
-        
+        int[] leftSum=countNode(root.left,count);
+        int[] rightSum=countNode(root.right,count);
         int totalCount= leftSum[0]+rightSum[0]+1;
         int totalSum=leftSum[1]+rightSum[1]+root.val;
+        if(totalSum/totalCount==root.val){
+            count[0]++;
+        }
         return new int[]{totalCount,totalSum};
     }
 }

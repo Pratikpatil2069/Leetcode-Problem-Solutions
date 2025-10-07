a50ba71ec46b351428202501a014e0c6356c62c9
Pratik Patil <pratikpatil2069@gmail.com>
2025-10-07 22:49:58 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,22 +1,17 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        PriorityQueue<Integer>queue=new PriorityQueue<>(new Comparator<>(){
-            public int compare(Integer a,Integer b){
-                return a-b;
-            }
-        });
-        for(int i=0;i<m;i++){
-            queue.add(nums1[i]);
+      int i=m-1;
+      int j=n-1;
+      int k=m+n-1;
+      while(i>=0 && j>=0){
+        if(nums1[i]>nums2[j]){
+            nums1[k--]=nums1[i--];
+        }else{
+            nums1[k--]=nums2[j--];
         }
-        for(int i=0;i<n;i++){
-            queue.add(nums2[i]);
-        }
-        int ind=0;
-
-        while(!queue.isEmpty()){
-            nums1[ind]=queue.poll();
-            ind++;
-        }
-        
+      }
+      while(j>=0){
+        nums1[k--]=nums2[j--];
+      }
     }
 }

2948ac9e6ad977aea45e956cd2ac947add4173b2
Pratik Patil <pratikpatil2069@gmail.com>
2025-10-09 20:35:59 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.16%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,17 +3,16 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         while(left<=right){
-            int mid=left+(right-left)/2;
+            int mid =left+(right-left)/2;
             if(nums[mid]==target){
                 return mid;
-            }
-            if(target>nums[mid]){
-                left=mid+1;
-            }else{
+            }else if(nums[mid]>target){
                 right=mid-1;
+            }else{
+                left=mid+1;
             }
         }
+       
         return left;
-        
     }
 }

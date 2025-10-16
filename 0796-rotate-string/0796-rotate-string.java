16eb5d3b6f78bb5b44374e5864904e4ae8c3ec0a
Pratik Patil <pratikpatil2069@gmail.com>
2025-10-16 14:22:20 +0530

    Time: 0 ms (100%), Space: 41.8 MB (21.67%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        StringBuilder sb=new StringBuilder(s);
+        for(int i=0;i<s.length();i++){
+            if(goal.equals(sb.toString())){
+                return true;
+            }
+            sb.append(sb.charAt(0));
+            sb.deleteCharAt(0);
+        }
+
+        return false;
+    }
+}

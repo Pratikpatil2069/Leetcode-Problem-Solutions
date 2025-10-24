7569a790607e072d3409dfd8f4848341cb54893a
Pratik Patil <pratikpatil2069@gmail.com>
2025-10-24 14:50:17 +0530

    Time: 9 ms (17.5%), Space: 45.7 MB (14.95%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,25 +1,18 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>>list=new ArrayList<>();
-        HashMap<Integer,Integer>map=new HashMap<>();
+        HashMap<Integer,List<Integer>>map=new HashMap<>();
         
         for(int i=0;i<groupSizes.length;i++){
-            map.put(i,groupSizes[i]);
-        }
-        for(int j=0;j<groupSizes.length;j++){
-            int val=groupSizes[j];
-            List<Integer>list1=new ArrayList<>();
-            for(int i=0;i<groupSizes.length;i++){
-               if(map.containsKey(i) && map.get(i)==val){
-                list1.add(i);
-                map.remove(i);
-                if(list1.size()>=groupSizes[j]){
-                    list.add(list1);
-                    break;
-                }
-               }
+            map.putIfAbsent(groupSizes[i],new ArrayList<>());
+            List<Integer>list1=map.get(groupSizes[i]);
+            list1.add(i);
+            if(map.get(groupSizes[i]).size()>=groupSizes[i]){
+                list.add(new ArrayList<>(map.get(groupSizes[i])));
+                map.get(groupSizes[i]).clear();
             }
         }
+       
         return list;
 
     }

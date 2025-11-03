a0f41f2fe1dac399664a55fde0911260239fe075
Pratik Patil <pratikpatil2069@gmail.com>
2025-11-03 19:43:21 +0530

    Time: 52 ms (9.7%), Space: 47 MB (9.45%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> list=new ArrayList<>();
-        for(int i=0;i<l.length;i++){
-            ArrayList<Integer>list1=new ArrayList<>();
-            for(int j=l[i];j<=r[i];j++){
+        List<Boolean> list = new ArrayList<>();
+        for (int i = 0; i < l.length; i++) {
+            ArrayList<Integer> list1 = new ArrayList<>();
+            for (int j = l[i]; j <= r[i]; j++) {
                 list1.add(nums[j]);
             }
-           
-            list.add(isArithmetic(list1));
-           
+            Collections.sort(list1);
+            boolean flag = true;
+            int dif = list1.get(1) - list1.get(0);
+            for (int k = 0; k < list1.size() - 1; k++) {
+                if ((list1.get(k + 1) - list1.get(k)) != dif) {
+                    flag = false;
+                    break;
+                }
+            }
+            list.add(flag);
+
         }
         return list;
     }
-    public boolean isArithmetic(ArrayList<Integer>list){
-        Collections.sort(list);
-        int dif=list.get(1)-list.get(0);
-        for(int i=0;i<list.size()-1;i++){
-            if((list.get(i+1)-list.get(i))!=dif){
-                return false;
-            }
-        }
-        return true;
-    }
+
 }

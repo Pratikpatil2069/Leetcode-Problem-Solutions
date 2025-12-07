27bdd82507a5312c402f33609a6e4d280d9ee59c
Pratik Patil <pratikpatil2069@gmail.com>
2025-12-07 20:43:37 +0530

    Time: 0 ms (100%), Space: 48.2 MB (45.67%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,10 +3,11 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         while(left<=right){
-            int mid=left+(right-left)/2;
+            int mid=(left+right)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(target>nums[mid]){
+            }
+            if(target>nums[mid]){
                 left=mid+1;
             }else{
                 right=mid-1;

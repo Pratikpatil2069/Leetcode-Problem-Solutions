402d17a266bcfd2fba8761e27a3e4fc169f0da7b
Pratik Patil <pratikpatil2069@gmail.com>
2026-01-02 09:37:09 +0530

    Time: 16 ms (42.3%), Space: 46.8 MB (43.1%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,34 +1,24 @@
 class Solution {
-    class pair{
-        char key;
-        int fre;
-        pair(int fre,char key){
-            this.key=key;
-            this.fre=fre;
-        }
-
-    }
     public String frequencySort(String s) {
         HashMap<Character,Integer>map=new HashMap<>();
-        StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++){
             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
         }
-        PriorityQueue<pair>pq=new PriorityQueue(new Comparator<pair>(){
-            public int compare(pair a,pair b){
-               return b.fre-a.fre;
+        PriorityQueue<int []>pq=new PriorityQueue<>(new Comparator<>(){
+            public int compare(int a[] ,int b[]){
+                return b[1]-a[1];
             }
-
         });
-        for(Character key:map.keySet()){
-            pq.add(new pair(map.get(key),key));
+        for(char key:map.keySet()){
+            pq.add(new int[]{(int)key,map.get(key)});
         }
+        StringBuilder sb=new StringBuilder();
         while(!pq.isEmpty()){
-            pair p=pq.poll();
-            for (int i = 0; i < p.fre; i++) {
-                sb.append(p.key);
+            int arr[]=pq.poll();
+            char ch=(char)arr[0];
+            for(int i=0;i<arr[1];i++){
+                sb.append(ch);
             }
-           
         }
         return sb.toString();
     }

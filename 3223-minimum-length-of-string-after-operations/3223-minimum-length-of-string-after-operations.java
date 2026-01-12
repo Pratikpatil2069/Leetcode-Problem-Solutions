cc593681ff019c88a1a0c224d6bf9b9748368725
Pratik Patil <pratikpatil2069@gmail.com>
2026-01-12 22:23:49 +0530

    Time: 87 ms (21.55%), Space: 48.5 MB (36.59%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
@@ -1,29 +1,23 @@
 class Solution {
     public int minimumLength(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        for(int i=1;i<sb.length()-1;i++){
-            char ch=sb.charAt(i);
-            int ind1=-1;
-            int ind2=-1;
-            for(int j=0;j<i;j++){
-                if(sb.charAt(j)==ch){
-                    ind1=j;
-                    break;
-                }
+       HashMap<Character,Integer>map=new HashMap<>();
+       for(int i=0;i<s.length();i++){
+        map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+       }
+       int ans=0;
+       for(int val:map.values()){
+        if(val>=3){
+            if(val%2==0){
+                ans+=2;
+            }else{
+                ans+=1;
             }
-            for(int k=i+1;k<sb.length();k++){
-                if(sb.charAt(k)==ch){
-                    ind2=k;
-                    break;
-                }
-            }
-            if(ind1!=-1 && ind2!=-1){
-                sb.deleteCharAt(ind2);
-                sb.deleteCharAt(ind1);
-                
-                i--;
-            }
+            
+        }else{
+            ans+=val;
         }
-     return sb.length();   
+       }
+       return ans;
     }
+
 }

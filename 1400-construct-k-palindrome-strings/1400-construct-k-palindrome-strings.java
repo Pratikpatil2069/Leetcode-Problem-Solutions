3f1d6de59a15dcc65c87d8a5cd446014738a8512
Pratik Patil <pratikpatil2069@gmail.com>
2026-01-20 07:35:32 +0530

    Time: 32 ms (8.11%), Space: 47.8 MB (21.02%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if(s.length()==k){
+            return true;
+        }
+        if(s.length()<k){
+            return false;
+        }
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        int count=0;
+        for(int value:map.values()){
+            if(value%2==1){
+                count++;
+            }
+        }
+        return count>k?false:true;
+    }
+}

76a5b8ef0069d7244f57260de89a4499651191c6
Pratik Patil <pratikpatil2069@gmail.com>
2026-01-31 21:43:17 +0530

    Time: 83 ms (0.1%), Space: 45.4 MB (17.46%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,16 +8,8 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        ListNode temp=node;
-        while(node.next!=null){
-             if(node.next.next==null){
-                temp=node;
-            }
-            node.val=node.next.val;
-            node=node.next;
-           
-        }
-        temp.next=null;
+        node.val=node.next.val;
+        node.next=node.next.next;
         
     }
 }

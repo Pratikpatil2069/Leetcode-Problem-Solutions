e4e7426e6fd7e56df37e964ab8c029727fd4ec8d
Pratik Patil <pratikpatil2069@gmail.com>
2026-02-01 09:11:54 +0530

    Time: 0 ms (100%), Space: 43.2 MB (11.46%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,12 +10,16 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode temp = head;
-        ListNode ptr = head;
-        while (ptr != null && ptr.next != null) {
-            temp = temp.next;
-            ptr = ptr.next.next;
+       ListNode temp=head;
+       ListNode ptr=head;
+       while(ptr!=null && ptr.next!=null){
+        ptr=ptr.next;
+        if(ptr==null){
+            return temp.next;
         }
-        return temp;
+        ptr=ptr.next;
+        temp=temp.next;
+       }
+       return temp;
     }
 }

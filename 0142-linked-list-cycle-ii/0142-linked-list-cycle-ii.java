714d54b07c315a7e2fa0b633edb318e1beea5f6c
Pratik Patil <pratikpatil2069@gmail.com>
2026-02-09 11:32:41 +0530

    Time: 0 ms (100%), Space: 46.8 MB (46.89%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -23,7 +23,7 @@ public class Solution {
                 fast=fast.next;
                 slow=slow.next;
             }
-            return fast;
+            return slow;
         }
        }
        return null;

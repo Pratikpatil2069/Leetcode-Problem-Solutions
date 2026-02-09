508c41f8efed86c7f5d998b14320c18bcf19e2fa
Pratik Patil <pratikpatil2069@gmail.com>
2026-02-09 14:17:41 +0530

    Time: 1 ms (99.7%), Space: 46.8 MB (16.59%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,35 +1,23 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null;
-        ListNode temp = null;
-        int carry = 0 ;
-        while(l1!=null || l2!=null || carry!=0) {
-            int val1 = (l1==null)?0:l1.val;
-            int val2 = (l2==null)?0:l2.val;
-            int value = val1 + val2 + carry;
-            ListNode Node = new ListNode(value%10);
-            carry = value/10;
-            if(head == null){
-                head = Node;
-                temp = Node;
-            }else {
-                temp.next = Node;
-                temp = temp.next;
-
+        ListNode dummy=new ListNode(-1);
+       ListNode temp=dummy;
+        int carry=0;
+        while(l1!=null || l2!=null || carry!=0){
+            int sum=carry;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
             }
-            if(l1!=null) l1 = l1.next;
-            if(l2!=null) l2 = l2.next;
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            ListNode newNode = new ListNode(sum%10);
+            temp.next=newNode;
+            temp=newNode;
+            carry=sum/10;
         }
-        return head;
+       return dummy.next;
     }
 }

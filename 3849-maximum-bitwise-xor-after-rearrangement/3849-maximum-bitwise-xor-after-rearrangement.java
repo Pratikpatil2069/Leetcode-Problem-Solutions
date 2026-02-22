86936906413a1f689e078b9646450b2d2cc1e6bd
Pratik Patil <pratikpatil2069@gmail.com>
2026-02-22 12:15:47 +0530

    Time: 66 ms (44.56%), Space: 54.2 MB (80.59%) - LeetHub

---
+++ 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String maximumXor(String s, String t) {
+
+        int countOfOnes = 0;
+
+        // Count 1s in t
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == '1') {
+                countOfOnes++;
+            }
+        }
+
+        int countOfZeros = s.length() - countOfOnes;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '1') {
+
+                if (countOfZeros > 0) {
+                    sb.append('1');
+                    countOfZeros--;
+                } else {
+                    sb.append('0');
+                }
+
+            } else {
+
+                if (countOfOnes > 0) {
+                    sb.append('1');
+                    countOfOnes--;
+                } else {
+                    sb.append('0');
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}

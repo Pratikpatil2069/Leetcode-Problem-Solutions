75b183901a71beaee00a2e0dcdc2b7f6787adc3d
Pratik Patil <pratikpatil2069@gmail.com>
2026-02-22 12:20:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.java
+++ 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.java
@@ -1,41 +1,33 @@
 class Solution {
     public String maximumXor(String s, String t) {
-
-        int countOfOnes = 0;
-
-        // Count 1s in t
-        for (int i = 0; i < t.length(); i++) {
+        long num=Long.parseLong(t,2);
+        int countOfOnes=0;
+         for (int i = 0; i < t.length(); i++) {
             if (t.charAt(i) == '1') {
                 countOfOnes++;
             }
         }
-
-        int countOfZeros = s.length() - countOfOnes;
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s.charAt(i) == '1') {
-
-                if (countOfZeros > 0) {
+        int countOfZeros=s.length()-countOfOnes;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                if(countOfZeros>0){
                     sb.append('1');
                     countOfZeros--;
-                } else {
+                }else{
                     sb.append('0');
+                    countOfOnes--;
                 }
-
-            } else {
-
-                if (countOfOnes > 0) {
+            }else{
+                 if(countOfOnes>0){
                     sb.append('1');
                     countOfOnes--;
-                } else {
+                }else{
                     sb.append('0');
+                    countOfZeros--;
                 }
             }
         }
-
         return sb.toString();
     }
 }

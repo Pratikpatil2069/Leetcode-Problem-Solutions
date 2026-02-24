4d2a784e03c05dcfed4494b64a5f26179a448cc2
Pratik Patil <pratikpatil2069@gmail.com>
2026-02-24 19:29:20 +0530

    Time: 8 ms (70.96%), Space: 47.2 MB (54.81%) - LeetHub

---
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.java
@@ -0,0 +1,55 @@
+class FrontMiddleBackQueue {
+    ArrayList<Integer>list;
+    public FrontMiddleBackQueue() {
+        list=new ArrayList<>();
+    }
+    
+    public void pushFront(int val) {
+        list.add(0,val);
+    }
+    
+    public void pushMiddle(int val) {
+        int ind=(list.size())/2;
+        list.add(ind,val);
+    }
+    
+    public void pushBack(int val) {
+        list.add(val);
+    }
+    
+    public int popFront() {
+        if(list.size()>0){
+            return list.remove(0);
+        }else{
+            return -1;
+        }
+    }
+    
+    public int popMiddle() {
+        if(list.size()>0){
+            int ind=(list.size()-1)/2;
+           return list.remove(ind);
+        } else{
+            return -1;
+        }       
+    }
+    
+    public int popBack() {
+        if(list.size()>0){
+            return list.remove(list.size()-1);
+        }else{
+            return -1;
+        }
+    }
+}
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue obj = new FrontMiddleBackQueue();
+ * obj.pushFront(val);
+ * obj.pushMiddle(val);
+ * obj.pushBack(val);
+ * int param_4 = obj.popFront();
+ * int param_5 = obj.popMiddle();
+ * int param_6 = obj.popBack();
+ */

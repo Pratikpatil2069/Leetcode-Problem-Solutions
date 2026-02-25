9bb2229a1017288f926586d33fcc599d21b8d4d8
Pratik Patil <pratikpatil2069@gmail.com>
2026-02-25 11:46:59 +0530

    Time: 50 ms (32.82%), Space: 132.2 MB (31.08%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,33 @@
+
+class LRUCache {
+
+    private int capacity;
+    private LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new LinkedHashMap<>();
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        int value = map.remove(key);
+        map.put(key, value);
+
+        return value;
+    }
+    
+    public void put(int key, int value) {
+
+        if (map.containsKey(key)) {
+            map.remove(key);
+        }else if (map.size() == capacity) {
+            int firstKey = map.keySet().iterator().next();
+            map.remove(firstKey);
+        }
+        map.put(key, value);
+    }
+}

218941b2307064a0ee3085dd07baec430fe4296b
Pratik Patil <pratikpatil2069@gmail.com>
2026-03-03 16:54:27 +0530

    Time: 4 ms (42.12%), Space: 44 MB (6.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                stack.push(s.charAt(i));
+            }else{
+               if(stack.isEmpty()){
+                return false;
+               }
+                char ch =s.charAt(i);
+                 if(ch == ')' && stack.peek() == '(') {
+                    stack.pop();
+                }
+                else if(ch == ']' && stack.peek() == '[') {
+                    stack.pop();
+                }
+                else if(ch == '}' && stack.peek() == '{') {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}

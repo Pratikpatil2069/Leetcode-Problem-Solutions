7de2beed80a513cf2ecdca10d144fc694bf9ae99
Pratik Patil <pratikpatil2069@gmail.com>
2026-03-26 10:31:21 +0530

    Time: 1 ms (93.98%), Space: 70.6 MB (84.94%) - LeetHub

---
+++ 2527-find-xor-beauty-of-array/2527-find-xor-beauty-of-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int xorBeauty(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor^=nums[i];
+        }
+        return xor;
+        
+    }
+}

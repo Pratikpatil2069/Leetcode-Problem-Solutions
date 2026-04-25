8e3368b3dbe4eaa74387b03acb646b25e64b8517
Pratik Patil <pratikpatil2069@gmail.com>
2026-04-25 13:34:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -4,20 +4,20 @@ class Solution {
             public int compare(int []a,int []b){
                 double fun1=(double)a[0]/a[1];
                 double fun2=(double)b[0]/b[1];
-                return Double.compare(fun1,fun2);
+                return Double.compare(fun2,fun1);
             }
         });
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
                 pq.add(new int[]{arr[i],arr[j]});
+                if(pq.size()>k){
+                    pq.poll();
+                }
             }
 
         }
-        while(k>1){
-            pq.poll();
-            k--;
-        }
-        return pq.poll();
+        
+        return pq.peek();
         
     }
 }

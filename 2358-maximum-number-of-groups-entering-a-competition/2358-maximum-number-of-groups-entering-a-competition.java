db8bf18e1d00e5f8c03b89b22cf7f54bbed4e453
Pratik Patil <pratikpatil2069@gmail.com>
2026-05-28 10:24:50 +0530

    Time: 1 ms (31.5%), Space: 76.1 MB (42.52%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
@@ -1,34 +1,5 @@
 class Solution {
     public int maximumGroups(int[] grades) {
-        // int count=0;
-        // Arrays.sort(grades);
-        // int left=0;
-        // int right=grades.length-1;
-        // int sum=0;
-        // int preSum=0;
-        // int count1=0;
-        // boolean flag=true;
-        // int preCount=0;
-        // while(left<=right){
-        //     if(flag){
-        //         sum+=grades[right--];
-        //         flag=false;
-        //     }else{
-        //         sum+=grades[left++];
-        //         flag=true;
-        //     }
-        //     count1++;
-        //     if(sum>preSum && count1>preCount){
-        //         preCount=count1;
-        //         count1=0;
-        //         preSum=sum;
-        //         sum=0;
-        //         count++;
-        //         flag=true;
-        //     }
-
-        // }
-        // return count;
         int count=0;
         int c=0;
         int prevC=0;

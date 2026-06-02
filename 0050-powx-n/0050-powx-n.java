3a3695141a04a69ccaf74900c0dda962ab639c03
Pratik Patil <pratikpatil2069@gmail.com>
2026-06-02 18:41:25 +0530

    Time: 0 ms (100%), Space: 48.2 MB (33.06%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        double ans=1;
+        long  nn=n;
+        if(nn<0){
+            nn=-1*nn;
+            return 1/pow(x,nn,ans);
+        }
+        return pow(x,nn,ans);
+        
+    }
+    public double pow(double x,double n,double ans){
+        if(n==0){
+            return ans;
+        }
+        if(n%2==0){
+            x=x*x;
+            n=n/2;
+        }else{
+            ans=ans*x;
+            n=n-1;
+        }
+        return pow(x,n,ans);
+        
+        
+    }
+}

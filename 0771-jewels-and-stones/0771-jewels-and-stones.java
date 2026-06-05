3d7a4219fb867783b72bdc2318724092ad174b6b
Pratik Patil <pratikpatil2069@gmail.com>
2026-06-05 08:38:52 +0530

    Time: 0 ms (100%), Space: 42.9 MB (90.3%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -2,7 +2,7 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
        int count=0;
         for(int i=0;i<stones.length();i++){
-            if(jewels.contains(stones.substring(i,i+1))){
+            if(jewels.indexOf(stones.charAt(i))!=-1){
                 count++;
             }
         }

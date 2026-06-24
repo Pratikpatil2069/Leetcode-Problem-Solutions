2c8103baac09c4734fc7f9e25e2782fc3bdd54eb
Pratik Patil <pratikpatil2069@gmail.com>
2026-06-24 11:39:38 +0530

    Time: 0 ms (100%), Space: 43 MB (98.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,41 +1,42 @@
 class Solution {
     public void sortColors(int[] nums) {
-        for(int i=0;i<nums.length-1;i++){
-            int num=nums[i];
-            int ind=i;
-            for(int j=i+1;j<nums.length;j++){
-                if(num>nums[j]){
-                   num=nums[j];
-                   ind=j;
-                }
+        int zero=0;
+        int one=0;
+        int two=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                zero++;
             }
-            int temp=nums[i];
-            nums[i]=num;
-            nums[ind]=temp;
+            if(nums[i]==1){
+                one++;
+            }
+            if(nums[i]==2){
+                two++;
+            }
         }
-        // int zero=0;
-        // int one=0;
-        // int two=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==0){
-        //         zero++;
+        int ind=0;
+        for(int i=0;i<zero;i++){
+            nums[ind++]=0;
+        }
+        for(int i=0;i<one;i++){
+            nums[ind++]=1;
+        }
+        for(int i=0;i<two;i++){
+            nums[ind++]=2;
+        }
+
+        // for(int i=0;i<nums.length-1;i++){
+        //     int num=nums[i];
+        //     int ind=i;
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(num>nums[j]){
+        //            num=nums[j];
+        //            ind=j;
+        //         }
         //     }
-        //     if(nums[i]==1){
-        //         one++;
-        //     }
-        //     if(nums[i]==2){
-        //         two++;
-        //     }
-        // }
-        // int ind=0;
-        // for(int i=0;i<zero;i++){
-        //     nums[ind++]=0;
-        // }
-        // for(int i=0;i<one;i++){
-        //     nums[ind++]=1;
-        // }
-        // for(int i=0;i<two;i++){
-        //     nums[ind++]=2;
+        //     int temp=nums[i];
+        //     nums[i]=num;
+        //     nums[ind]=temp;
         // }
        
     }

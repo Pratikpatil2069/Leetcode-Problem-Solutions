262b5571660e20d2deede396eb2701d3cf0c8c7c
Pratik Patil <pratikpatil2069@gmail.com>
2026-07-01 16:05:18 +0530

    Time: 15 ms (17.23%), Space: 46.8 MB (18.44%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
@@ -1,24 +1,26 @@
 class Solution {
     public char kthCharacter(int k) {
-        String s="a";
-        StringBuilder sb=new StringBuilder();
-         sb.append(s);
-        return fun(k,sb);
+       return kthChar(k,"a");
     }
-    public char fun(int k,StringBuilder sb){
-         if(sb.length()>=k){
-            return sb.charAt(k-1);
+    public char kthChar(int k,String str){
+        if(str.length()>=k){
+            return str.charAt(k-1);
         }
-        String s1=sb.toString();
-         for(int i=0;i<s1.length();i++){
-            char ch=s1.charAt(i);
-            if(ch=='z'){
-                ch='a';
-            }else{
-                ch=(char)(ch+1);
-            }
-            sb.append(ch);
-         }
-        return fun(k,sb);
+        str+=nextString(str,"",0);
+        return kthChar(k,str);
     }
+    public String nextString(String str,String s,int ind){
+        if(ind>=str.length()){
+            return s;
+        }
+        char ch=str.charAt(ind);
+        if(ch=='z'){
+            ch='a';
+        }else{
+            ch++;
+        }
+        return nextString(str,s+ch,ind+1);
+        
+    }
+        
 }

65632bad601990e150d326178ce94e8b2594e68a
Pratik Patil <pratikpatil2069@gmail.com>
2026-07-13 09:52:09 +0530

    Time: 0 ms (100%), Space: 44.1 MB (88.5%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,30 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-
-        // Make sure list1 starts with the smaller value
-        if (list1.val > list2.val) {
-            ListNode temp = list1;
-            list1 = list2;
-            list2 = temp;
+        
+        return merge(list1,list2);
+        
+    }
+    private ListNode merge(ListNode list1,ListNode list2){
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+            return list1;
         }
 
-        ListNode head = list1;
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-
-        while (temp1 != null && temp2 != null) {
-            if (temp1.next == null || temp1.next.val > temp2.val) {
-                // Insert temp2 between temp1 and temp1.next
-                ListNode nextTemp2 = temp2.next;
-                temp2.next = temp1.next;
-                temp1.next = temp2;
-                temp2 = nextTemp2;
-            }
-            temp1 = temp1.next;
+        if(list1.val<=list2.val){
+          list1.next=merge(list1.next,list2);
+          return list1;
+        }else{
+          list2.next=merge(list1,list2.next);
+          return list2;
         }
-
-        return head;
     }
 }

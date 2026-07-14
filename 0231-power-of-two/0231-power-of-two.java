7cd23258472b34f64477c2b190d61157c3bc21b7
Pratik Patil <pratikpatil2069@gmail.com>
2026-07-14 10:57:16 +0530

    Time: 1 ms (96.67%), Space: 42.1 MB (97.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n>0 && Integer.bitCount(n)==1;
+        return check(n);
+    }
+    private boolean check(int n){
+        if(n<=0){
+            return false;
+        } 
+        if(n==1){
+            return true;
+        }
+        if(n%2!=0){
+            return false;
+        }
+        return check(n/2);
+        
     }
 }
